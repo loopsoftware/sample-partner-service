eb33678d13c904794292f1190098fdbf2c86e1fa
AlexYup <alex@loopsoftware.fr>
2018-09-14 18:29:31 +0100

    FY-998 server function call example (#1)

--- main.js
+++ main.js
@@ -3,9 +3,34 @@ const utils = require('./utils.js');
 
 Object.assign(exports, require("./index.js"));
 
-async function getInvoices(session) {
-    return utils.dbFind(session, "user", "nsC", yQuery.From("invoicing.invoice").Select().Take(20).Execute());
+/**
+ * example of yQuery
+ *
+ * @param {YSession} session session
+ * @param {string} conn connection name
+ * @return {Promise<object>} result of find
+ */
+async function getInvoices(session, conn) {
+    return utils.dbFind(session, "user", conn, yQuery.From(`invoicing.invoice`).Select().Take(20).Execute());
 }
+
+
+/**
+ * example of service function call
+ *
+ * @param {serverRequestDescription} serv request
+ * @param {YSession} session session
+ * @param {string} invoiceId invoice ID
+ * @return {Promise<object>} result of the invoicing/total/total function
+ */
+async function getTotals(serv, session, invoiceId) {
+    return (await utils.callFunction(serv, session, {
+        module: "invoicing",
+        command: "total",
+        args: {invoiceID: invoiceId}
+    })).data;
+}
+
 
 // overwrite default webView
 exports.webView = async function (
@@ -19,11 +44,21 @@ exports.webView = async function (
     subscriptionState,
     context
 ) {
-    let invoices = {};
+    let invoices = {},
+        total = {};
     const session = await utils.getSession(context.sessionId);
     if (session) {
-        invoices = await getInvoices(session);
+        const conn = `wv${dbId}`;
+        if (!session.connection(conn)) {
+            await session.dbserver.connect(conn, dbId);
+        }
+
+        [invoices, total] = await Promise.all([
+            getInvoices(session, conn),
+            getTotals(serv, session, "0ae4ca94-463d-471f-9ab1-f907f5d1ba59") // example invoiceId
+        ]);
     }
+
 
     return {
         html: viewports.reduce((html, dimension) => {
@@ -38,6 +73,7 @@ exports.webView = async function (
         <li>${subscriptionState}</li>
         <li>${JSON.stringify(context)}</li>
         <li>${JSON.stringify(invoices)}</li>
+        <li>${JSON.stringify(total)}</li>
     </ul>
 </body>
 </html>`;
