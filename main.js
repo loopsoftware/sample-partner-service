e9ce546d06f46f0350dbe282b0be8efbe0e7340f
AlexYup <alex@loopsoftware.fr>
2018-09-19 15:39:08 +0100

    FY-998 example of external API call (#2)

--- main.js
+++ main.js
@@ -24,13 +24,47 @@ async function getInvoices(session, conn) {
  * @return {Promise<object>} result of the invoicing/total/total function
  */
 async function getTotals(serv, session, invoiceId) {
-    return (await utils.callFunction(serv, session, {
+    return (await utils.callFunction(serv, session, "user", {
         module: "invoicing",
         command: "total",
         args: {invoiceID: invoiceId}
     })).data;
 }
 
+
+/**
+ * example of external API call
+ *
+ * @param {serverRequestDescription} serv request
+ * @param {YSession} session session
+ * @param {string} domain domain
+ * @param {string} dbId database
+ * @return {Promise<responseLocal>} user info json
+ */
+async function getUserInfo(serv, session, domain, dbId) {
+    const role = await utils.getRoleByName(session, "user");
+    try {
+        return (await fetch(
+            "https://quipu.auth0.com/userinfo", // external API endpoint
+            {
+                serviceProvider: {
+                    name: "test-partner",  // service name
+                    domain: domain,
+                    dbId: dbId
+                }
+            },
+            session,
+            role,
+            serv
+        )).json();
+    } catch (error) {
+        if (error.idp) {
+            error.loginPrompt = utils.defaultLoginPrompt(serv, session, role, error.idp, "Quipu Inc");
+        }
+        throw error;
+    }
+
+}
 
 // overwrite default webView
 exports.webView = async function (
@@ -45,7 +79,8 @@ exports.webView = async function (
     context
 ) {
     let invoices = {},
-        total = {};
+        total = {},
+        userInfo = {};
     const session = await utils.getSession(context.sessionId);
     if (session) {
         const conn = `wv${dbId}`;
@@ -53,12 +88,19 @@ exports.webView = async function (
             await session.dbserver.connect(conn, dbId);
         }
 
-        [invoices, total] = await Promise.all([
-            getInvoices(session, conn),
-            getTotals(serv, session, "0ae4ca94-463d-471f-9ab1-f907f5d1ba59") // example invoiceId
-        ]);
+        try {
+            [invoices, total, userInfo] = await Promise.all([
+                getInvoices(session, conn),
+                getTotals(serv, session, "0ae4ca94-463d-471f-9ab1-f907f5d1ba59"), // example invoiceId
+                getUserInfo(serv, session, domain, dbId)
+            ]);
+        } catch (e) {
+            if (e.loginPrompt) {
+                return {html: {default: e.loginPrompt}}
+            }
+            throw e;
+        }
     }
-
 
     return {
         html: viewports.reduce((html, dimension) => {
@@ -74,6 +116,7 @@ exports.webView = async function (
         <li>${JSON.stringify(context)}</li>
         <li>${JSON.stringify(invoices)}</li>
         <li>${JSON.stringify(total)}</li>
+        <li>${JSON.stringify(userInfo)}</li>
     </ul>
 </body>
 </html>`;
